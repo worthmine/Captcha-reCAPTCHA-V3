d834296695223ec62174871d1f6b5971191d7ce0
worthmine <worthmine@cpan.org>
2020-02-27 18:02:14 +0900

    added the POD

--- lib/CAPTCHA/reCAPTCHA/V3.pm
+++ lib/CAPTCHA/reCAPTCHA/V3.pm
@@ -84,15 +84,66 @@ __END__
 
 =head1 NAME
 
-Captcha::reCAPTCHA::V3 - It's new $module
+Captcha::reCAPTCHA::V3 - A Perl implementation of reCAPTCHA API version v3
 
 =head1 SYNOPSIS
 
-    use Captcha::reCAPTCHA::V3;
+Captcha::reCAPTCHA::V3 provides you to integrate Google reCAPTCHA v3 for your web applications.
+
+ use Captcha::reCAPTCHA::V3;
+ my $rc = Captcha::reCAPTCHA::V3->new(
+     secret  => '__YOUR_SECRET__',
+     sitekey => '__YOUR_SITEKEY__',
+ );
+
+ ...
+ 
+ my $content = $rc->verify($param{'reCAPTCHA_Token'});
+ if( $content->{'success'} ){
+    # code for succeeding
+ }else{
+    # code for failing
+ }
 
 =head1 DESCRIPTION
 
-CAPTCHA::reCAPTCHA::V3 is ...
+Captcha::reCAPTCHA::V3 is inspired from L<Captcha::reCAPTCHA::V2|https://metacpan.org/pod/Captcha::reCAPTCHA::V2>
+
+This one is especially for Google reCAPTCHA v3, not for v2 because APIs are so defferent.
+
+=head2 Basic Usage
+
+=head3 new()
+
+Requires secret and sitekey when constructing.
+You have to get them before running from L<here|https://www.google.com/recaptcha/intro/v3.html>
+
+=head3 verify()
+
+Requires just only response key being got from Google reCAPTCHA API.
+B<DO NOT> add remote address. there is no function for remote address in reCAPTCHA v3
+
+=head2 Additional method for lazy persons(not supported)
+
+=head3 script4head()
+
+You can insert this in your E<lt>headE<gt> tag
+
+=head3 input4form
+
+You can insert this in your E<lt>formE<gt> tag
+
+=head1 SEE ALSO
+
+=over
+
+=item L<Captcha::reCAPTCHA::V2|https://metacpan.org/pod/Captcha::reCAPTCHA::V2>
+
+=item L<Google reCAPTCHA v3|https://www.google.com/recaptcha/intro/v3.html>
+
+=item L<Google reCAPTCHA v3 API document|https://developers.google.com/recaptcha/docs/v3>
+
+=back
 
 =head1 LICENSE
 
@@ -103,7 +154,7 @@ it under the same terms as Perl itself.
 
 =head1 AUTHOR
 
-worthmine E<lt>worthmine@cpan.orgE<gt>
+worthmine E<lt>worthmine@gmail.comE<gt>
 
 =cut
 
